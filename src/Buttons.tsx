f735450208579979f03b2636202825b0fb386038
m19e <nexus.me7290@gmail.com>
2020-11-30 23:32:58 +0900

    Add buttons

---
+++ src/Buttons.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "ui-neumorphism";
+
+const Buttons = () => (
+    <>
+        <Button style={{ margin: "8px" }}>1</Button>
+        <Button style={{ margin: "8px" }}>2</Button>
+        <Button style={{ margin: "8px" }}>3</Button>
+        <Button style={{ margin: "8px" }}>4</Button>
+        <Button style={{ margin: "8px" }}>5</Button>
+    </>
+);
+
+export default Buttons;
