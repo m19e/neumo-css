5ccbfcba8f1ef1dbb4c5eca5c5f4cd2e107086ba
m19e <nexus.me7290@gmail.com>
2020-12-01 00:13:22 +0900

    Add divider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Buttons from "./Buttons";
+import { Divider } from "ui-neumorphism";
 import "ui-neumorphism/dist/index.css";
 import "./App.css";
 
 const App = () => (
     <div style={{ flex: 1 }}>
         {["row", "row-reverse", "column", "column-reverse"].map((fd: any) => (
-            <div style={{ display: "flex", flexDirection: fd }}>
-                <Buttons />
-            </div>
+            <>
+                <div style={{ display: "flex", flexDirection: fd }}>
+                    <Buttons />
+                </div>
+                <Divider dense style={{ margin: "8px" }} />
+            </>
         ))}
     </div>
 );
