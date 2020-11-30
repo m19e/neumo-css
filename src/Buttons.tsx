e57283ab868f25e7a30ef466818640c7a57ddc20
m19e <nexus.me7290@gmail.com>
2020-12-01 01:01:23 +0900

    Inject button width

--- src/Buttons.tsx
+++ src/Buttons.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Button } from "ui-neumorphism";
 
-const Buttons = () => (
+const Buttons = ({ w = "100px" }) => (
     <>
         {[1, 2, 3, 4, 5].map((n) => (
-            <Button rounded style={{ margin: "8px", width: "100px" }}>
+            <Button rounded style={{ margin: "8px", width: w }}>
                 {n}
             </Button>
         ))}
