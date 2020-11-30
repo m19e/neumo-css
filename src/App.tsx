ce9ab135e1ab4b85a3576847f708f593594d9ed8
m19e <nexus.me7290@gmail.com>
2020-12-01 02:13:29 +0900

    Set center, left center

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Buttons from "./Buttons";
-import { Alert, Divider } from "ui-neumorphism";
+import { Alert, Button, Divider } from "ui-neumorphism";
 import "ui-neumorphism/dist/index.css";
 import "./App.css";
 
 const App = () => (
     <div style={{ flex: 1, backgroundColor: "#e4ebf5" }}>
+        <Alert inset type="success">
+            Center & Left Center
+        </Alert>
+        <div style={{ display: "flex" }}>
+            <div style={{ flexGrow: 1, width: "100%", display: "flex", justifyContent: "flex-end" }}>
+                <Button>Left Center</Button>
+            </div>
+            <Button style={{ flexGrow: 0, width: "150px" }}>Center</Button>
+            <div style={{ flexGrow: 1, width: "100%" }}></div>
+        </div>
+        <Divider dense style={{ margin: "8px" }} />
+
         <Alert inset type="success">
             Flex Direction
         </Alert>
@@ -48,6 +60,18 @@ const App = () => (
         {["flex-start", "flex-end", "center", "stretch", "space-between", "space-around"].map((ac: any) => (
             <>
                 <div style={{ display: "flex", flexWrap: "wrap", height: "200px", alignContent: ac }}>
+                    <Buttons w={"200px"} />
+                </div>
+                <Divider dense style={{ margin: "8px" }} />
+            </>
+        ))}
+
+        <Alert inset type="success">
+            Flex Wrap
+        </Alert>
+        {["nowrap", "wrap", "wrap-reverse"].map((fw: any) => (
+            <>
+                <div style={{ display: "flex", flexWrap: fw }}>
                     <Buttons w={"200px"} />
                 </div>
                 <Divider dense style={{ margin: "8px" }} />
