5644c7b132c375a0847c54cc09ac0048533fd255
m19e <nexus.me7290@gmail.com>
2020-11-30 23:36:35 +0900

    Display flex row

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { Button } from "ui-neumorphism";
+import Buttons from "./Buttons";
 import "ui-neumorphism/dist/index.css";
+import "./App.css";
 
-const App = () => <Button />;
+const App = () => (
+    <div style={{ flex: 1 }}>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+            <Buttons />
+        </div>
+    </div>
+);
 
 export default App;
